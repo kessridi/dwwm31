0ebd70cae0af031a2c25fe743edbb349db36ac8c
kessridi <kamel.essridi.formation@gmail.com>
2022-11-30 14:04:28 +0100

    temp

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Fichier PHP</h1>
+    <?PHP
+//
+echo "ceci est une ligne de code PHP";
+echo"<br>";
+
+echo "cette fois, mon PHP est <strong> interpreté </strong>";
+echo"<br>";
+echo"<br>";
+
+$resultat = 5+8;
+echo "le résultat de l'addition 5+8 est : ".$resultat. "<br>";
+
+echo "le résultat de l'addition  7+8 est : ".(7+8). "<br>";
+
+   ?>
+</body>
+
+</html>
